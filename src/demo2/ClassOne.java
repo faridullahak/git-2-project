1c64515b5604e639bd5b8c41949d59e10719ced2
faridullahak <faridullah.akbari@tekschool.us>
2023-08-18 11:34:22 -0400

    Allah is mercyful

---
+++ src/demo2/ClassOne.java
@@ -0,0 +1,10 @@
+package demo2;
+
+public class ClassOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is my friday class");
+	}
+
+}
